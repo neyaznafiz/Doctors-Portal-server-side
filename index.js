df7e25766708627630608be9c0d80cc9cf047fd2
niiyazobug <niiyaz5555@gmail.com>
2022-05-14 01:37:53 +0600

    booking service info post method api impliment

--- index.js
+++ index.js
@@ -20,12 +20,29 @@ async function run() {
     try {
         await client.connect()
         const serviceCollection = client.db('doctorst_portal').collection('services')
+        const bookingCollection = client.db('doctorst_portal').collection('bookings')
 
         app.get('/service', async (req, res) => {
             const query = {}
             const services = await serviceCollection.find(query).toArray()
             res.send(services)
         })
+
+        /**
+         * API Naving Convention
+         * app.get('/booking') // get all bokking in this collection. or get more than one, or by filter
+         * app.get('/booking/:id') // get a specific booking
+         * app.post('/booking') // add a new booking
+         * app.patch('/booking/:id') // 
+         * app.delete('/booking/:id') // 
+        */
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body
+            const result = await bookingCollection.insertOne(booking)
+            res.send(result)
+        })
+
     }
     finally { }
 }
