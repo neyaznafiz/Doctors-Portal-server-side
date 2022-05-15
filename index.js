2d3f0a50a5c21362db99f0537746b923411624d4
niiyazobug <niiyaz5555@gmail.com>
2022-05-15 12:09:23 +0600

    put api created for user filter by email

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ async function run() {
         })
 
 
-        app.get('/user/:email', async (req, res) => {
+        app.put('/user/:email', async (req, res) => {
             const email = req.params.email
             const user = req.body
             const filter = { email: email }
