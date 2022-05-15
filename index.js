96b2e36ee82c618bfc8ed70773f9f654f86c9908
niiyazobug <niiyaz5555@gmail.com>
2022-05-15 19:15:56 +0600

    find amin by email api created

--- index.js
+++ index.js
@@ -54,15 +54,31 @@ async function run() {
             res.send(users)
         })
 
+        // find admin by email api
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email
+            const user = await usersCollection.findOne({email: email})
+            const isAdmin = user.role === 'admin'
+            res.send({admin: isAdmin})
+        })
+
         // make admin api
         app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email
-            const filter = { email: email }
-            const updatedDoc = {
-                $set: { role: 'admin' },
+            const requester = req.decoded.email
+            const requesterAccount = await usersCollection.findOne({ email: requester })
+
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email }
+                const updatedDoc = {
+                    $set: { role: 'admin' },
+                }
+                const result = await usersCollection.updateOne(filter, updatedDoc)
+                res.send(result)
             }
-            const result = await usersCollection.updateOne(filter, updatedDoc)
-            res.send(result)
+            else {
+                res.status(403).send({ message: 'Forbidden Access' })
+            }
         })
 
 
